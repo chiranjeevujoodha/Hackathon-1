34c57a65a241b94d16b23120bff8303ac598ddc5
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2023-11-26 10:50:40 +0400

    new

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-function interested(button){
 
-    button.style.backgroundColor = "#28a745"
+var flag = 0
+
+function interested(button){
+    // var btn = document.querySelector('#btn1')
+
+
+    if (flag == 0){
+        button.style.backgroundColor = 'green'
+        flag = 1
+    } else {
+        button.style.backgroundColor = '#0056b3'
+        flag = 0
+    }
     
 }
+
