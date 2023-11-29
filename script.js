83afec34c8851988240f7e9ae6514d9addfdf594
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2023-11-29 14:02:17 +0400

    new

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
 
-var flag = 0
-
 function interested(button){
     // var btn = document.querySelector('#btn1')
 
-
-    if (flag == 0){
+    console.log(button.style.backgroundColor)
+    if(button.style.backgroundColor == '' || button.style.backgroundColor == 'rgb(0, 86, 179)') {
         button.style.backgroundColor = 'green'
-        flag = 1
     } else {
         button.style.backgroundColor = '#0056b3'
-        flag = 0
     }
+
+
+    // var flag = 0
+
+    // if (flag == 0) {
+    //     button.style.backgroundColor = 'green'
+    //     flag = 1
+    // } else {
+    //     button.style.backgroundColor = '#0056b3'
+    //     flag = 0
+    // }
     
 }
 
